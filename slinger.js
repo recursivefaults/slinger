c778b4d3f07b663bf380b09359a896c7990f9ff1
Ryan Latta <recursive.faults@gmail.com>
2014-01-20 17:36:17 -0500

    Silly js loading issue

---
+++ slinger.js
@@ -0,0 +1,10 @@
+    "use strict"
+    var Q = Quintus({development: true});
+    Q = Q.include("Sprites, Input, Scenes");
+    Q = Q.setup("slinger-canvas", {
+        width: 640,
+        height: 480
+    });
+    
+
+
