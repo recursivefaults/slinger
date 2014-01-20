9f0860252a80c1269de9b6b4fda7e263e633b61d
Ryan Latta <recursive.faults@gmail.com>
2014-01-20 18:02:27 -0500

    Drawing a square

--- slinger.js
+++ slinger.js
@@ -1,10 +1,38 @@
+(function() {
     "use strict"
     var Q = Quintus({development: true});
-    Q = Q.include("Sprites, Input, Scenes");
-    Q = Q.setup("slinger-canvas", {
+    Q.include("Sprites, Input, Scenes");
+    Q.setup("slinger-canvas", {
         width: 640,
         height: 480
     });
+
+
+    Q.Sprite.extend("Player", {
+        init: function(p) {
+                  this._super(p, {
+                      color: "red",
+                      w: 50,
+                      h: 70
+                  });
+              },
+        draw: function(ctx) {
+                  ctx.fillStyle = this.p.color;
+                  ctx.fillRect(-this.p.cx,
+                      -this.p.cy,
+                      this.p.w,
+                      this.p.h);
+              }
+
+    });
+
+    var player = new Q.Player();
+    Q.gameLoop(function(deltaTime) {
+        Q.clear();
+        player.render(Q.ctx);
+
+    });
+})();
     
 
 
