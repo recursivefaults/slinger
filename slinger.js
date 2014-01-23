355f204db56482c2ec5309d6d17c1d8a7e536154
Ryan Latta <recursive.faults@gmail.com>
2014-01-22 21:37:06 -0500

    Tilesheets and shit

--- slinger.js
+++ slinger.js
@@ -3,27 +3,23 @@
     var _kSlowdown = 0.05
     var _kVelocity_x_max = 300.0
     var Q = Quintus({development: true});
-    Q=Q.include("Sprites, Input, Scenes, Anim");
+    Q=Q.include("Sprites, Input, Scenes, Anim, 2D");
     Q=Q.setup("slinger-canvas");
     Q=Q.controls();
 
 
 
     Q.scene("InitialScene", function(stage) {
+        var level = new Q.TileLayer({dataAsset:"levels.json", sheet: "tiles"});
         var player = new Q.Player({x: 400, y: 300});
         stage.insert(player);
+        stage.insert(level);
     });
 
-    Q.load(["Simon.png", "simon.json"], function() {
+    Q.load(["Simon.png", "simon.json", "tiles.png", "levels.json"], function() {
         Q.compileSheets("Simon.png", "simon.json");
+        Q.sheet("tiles", "tiles.png", {tileW: 32, tileH: 32});
         Q.stageScene("InitialScene");
-        Q.gameLoop(function(deltaTime) {
-            Q.clear();
-            var player = Q("Player").first();
-            player.update(deltaTime);
-            player.render(Q.ctx);
-
-        });
     });
     
 
